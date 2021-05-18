5de3ddef756cda076bba38eff19d540e6c08b660
Ricardo Montenegro <ramn21@gmail.com>
2021-05-18 18:49:21 +0200

    Add expense in errorReducer

--- src/reducers/manageErrors.js
+++ src/reducers/manageErrors.js
@@ -4,6 +4,8 @@ const errorReducer = (state = [], action) => {
   switch (action.type) {
     case types.LOGGIN_ERROR:
       return { loggingError: action.payload };
+    case types.EXPENSES_LOAD_ERROR:
+      return { expensesError: action.payload };
     default:
       return state;
   }
