3c1eea70728b916202c57d48fb001cb1adbc5f47
Ricardo Montenegro <ramn21@gmail.com>
2021-05-18 18:44:23 +0200

    Add routes for sign in, sign up and profile

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
-function App() {
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { checkTokenUser } from '../actions/logginActions';
+import Home from '../container/Home';
+import SignUpForm from '../container/SingUpForm';
+import SignInForm from '../container/SignInForm';
+import Profile from '../container/Profile';
+
+function App({ userToken }) {
+  useEffect(() => {
+    userToken();
+  }, []);
   return (
-    <div>Hello this is a test</div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/sign_up" component={SignUpForm} />
+      <Route exact path="/sign_in" component={SignInForm} />
+      <Route exact path="/profile" component={Profile} />
+    </Switch>
   );
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  userToken: () => dispatch(checkTokenUser()),
+});
+
+App.propTypes = {
+  userToken: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(App);
