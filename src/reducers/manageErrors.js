62cf70714c6061811f5d8edc6bd93946a338364c
Ricardo Montenegro <ramn21@gmail.com>
2021-05-19 14:08:37 +0200

    Add expenses dog to error reducer

--- src/reducers/manageErrors.js
+++ src/reducers/manageErrors.js
@@ -6,6 +6,12 @@ const errorReducer = (state = [], action) => {
       return { loggingError: action.payload };
     case types.EXPENSES_LOAD_ERROR:
       return { expensesError: action.payload };
+    case types.EXPENSE_CREATED_ERROR:
+      return { expenseError: action.payload };
+    case types.DOGS_LOAD_ERROR:
+      return { dogsError: action.payload };
+    case types.DOG_CREATED_ERROR:
+      return { dogError: action.payload };
     default:
       return state;
   }
