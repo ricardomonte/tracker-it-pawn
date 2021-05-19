6bc18e53c51f50e866b000dbc0144b677410e24a
Ricardo Montenegro <ramn21@gmail.com>
2021-05-19 14:07:58 +0200

    Add dogs and createdReducer to rootreducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 import authenticatedUser from './authorization';
 import manageErr from './manageErrors';
 import expenses from './expenses';
+import dogs from './dogs';
+import createdReducer from './createdSuccess';
 
 const rootReducer = combineReducers({
   authenticatedUser,
   expenses,
+  dogs,
+  createdReducer,
   manageErr,
 });
 
