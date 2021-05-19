d3d5f48382e010e6859063abf58586d9e95844c5
Ricardo Montenegro <ramn21@gmail.com>
2021-05-19 03:57:54 +0200

    Add actionCreator for expenses

--- src/actions/expensesActions.js
+++ src/actions/expensesActions.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import * as types from './actionTypes';
 
-const url = 'http://localhost:4000/api/v1/expenses'
+const url = 'http://localhost:4000/api/v1/expenses';
+
+export const expenseCreatedSuccess = (expenses) => ({
+  type: types.EXPENSE_CREATED_SUCCESS,
+  payload: expenses,
+});
 
 export const expensesLoadSuccess = (expenses) => ({
   type: types.EXPENSES_LOAD_SUCCESS,
@@ -13,13 +18,33 @@ export const expensesLoadError = (expenses) => ({
   payload: expenses,
 });
 
+export const expenseCreatedError = (expenses) => ({
+  type: types.EXPENSE_CREATED_ERROR,
+  payload: expenses,
+});
+
 export const getExpenses = () => (dispatch) => {
   const auth = localStorage.token;
   axios.get(url, {
     headers: { Authorization: `Bearer ${auth}` },
   }).then((response) => {
-    dispatch(expensesLoadSuccess(response.data))
+    dispatch(expensesLoadSuccess(response.data));
   }).catch((error) => {
-    dispatch(expensesLoadError(error.response.data.error))
-  })
-}
+    dispatch(expensesLoadError(error.response.data.error));
+  });
+};
+
+export const createExpense = (expenses) => (dispatch) => {
+  const auth = localStorage.token;
+  const data = {
+    category: expenses.category,
+    detail: expenses.detail,
+    amount: expenses.amount,
+  };
+
+  axios.post(url, data, { headers: { Authorization: `Bearer ${auth}` } }).then((response) => {
+    dispatch(expenseCreatedSuccess(`${response.data.category} created`));
+  }).catch((error) => {
+    dispatch(expenseCreatedError(error.response.data.error));
+  });
+};
