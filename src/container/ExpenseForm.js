e1d32f06958844c6688398beea745df008b346f3
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 23:02:24 +0200

    Fix expense form

--- src/container/ExpenseForm.js
+++ src/container/ExpenseForm.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { useHistory, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { createExpense } from '../actions/expensesActions';
+import TrackerStyle from '../styles/Forms.module.css';
 
 const useDidUpdate = (callback, deps) => {
   const hasMount = useRef(false);
@@ -41,12 +42,11 @@ const ExpenseForm = ({ expenseCreate, expenseSuccess }) => {
     setExpense(updateExpense);
   };
 
-  const handleValidation = (event) => {
+  const handleValidation = () => {
     if (expense.category === '' || expense.detail === '' || expense.amount === '') {
       setInvalidform(true);
       return;
     }
-    event.preventDefault();
     const amountNumeric = parseInt(expense.amount, 10);
     setExpense({ ...expense, amount: amountNumeric });
     expenseCreate(expense);
@@ -57,31 +57,40 @@ const ExpenseForm = ({ expenseCreate, expenseSuccess }) => {
   };
 
   return (
-    <div>
-      <form>
-        <label htmlFor="category">
+    <div className={TrackerStyle.containerTotal}>
+      <form className={TrackerStyle.container}>
+        <label htmlFor="category" className={TrackerStyle.label}>
           Category:
-          <input id="category" type="text" value={expense.category} name="category" onChange={handleChange} placeholder="E.g. Food" />
+          <input className={TrackerStyle.inputs} id="category" type="text" value={expense.category} name="category" onChange={handleChange} placeholder="E.g. Food" />
         </label>
-        <label htmlFor="detail">
+        <label htmlFor="detail" className={TrackerStyle.label}>
           Detail:
-          <input id="detail" type="text" value={expense.detail} name="detail" onChange={handleChange} placeholder="E.g. Buy food for fido" />
+          <input className={TrackerStyle.inputs} id="detail" type="text" value={expense.detail} name="detail" onChange={handleChange} placeholder="E.g. Buy food for fido" />
         </label>
-        <label htmlFor="amount">
+        <label htmlFor="amount" className={TrackerStyle.label}>
           Amount:
-          <input id="amount" type="number" value={expense.amount} name="amount" onChange={handleChange} />
+          <input className={TrackerStyle.inputs} id="amount" type="number" value={expense.amount} name="amount" onChange={handleChange} />
         </label>
-        <label htmlFor="datePayment">
-          Amount:
-          <input id="datePayment" type="date" value={expense.datePayment} name="datePayment" onChange={handleChange} />
+        <label htmlFor="datePayment" className={TrackerStyle.label}>
+          When did you make it:
+          <input className={TrackerStyle.inputs} id="datePayment" type="date" value={expense.datePayment} name="datePayment" onChange={handleChange} />
         </label>
-        <button type="submit" onClick={handleValidation}>Submit</button>
+        <button type="button" onClick={handleValidation} className={TrackerStyle.btn}>Submit</button>
       </form>
       {
       invalidForm
-        ? <div onMouseOver={handleSpan} onFocus={handleSpan}>invalid formw</div>
+        ? (
+          <div
+            onMouseOver={handleSpan}
+            onFocus={handleSpan}
+            className={TrackerStyle.invalid}
+          >
+            invalid form
+          </div>
+        )
         : <div />
       }
+      <Link to="/profile" className={TrackerStyle.back}>Back</Link>
     </div>
   );
 };
