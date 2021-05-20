4c870b7c002dd31f94aedcf477419e2bfb52d8c7
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 22:58:26 +0200

    Add actioncretor to get one dog, patch one dog

--- src/actions/dogsActions.js
+++ src/actions/dogsActions.js
@@ -18,9 +18,29 @@ export const dogsLoadError = (dogs) => ({
   payload: dogs,
 });
 
+export const oneDogLoadError = (dogs) => ({
+  type: types.ONE_DOG_LOAD_ERROR,
+  payload: dogs,
+});
+
+export const oneDogLoadSuccess = (dog) => ({
+  type: types.DOG_LOAD_SUCCESS,
+  payload: dog,
+});
+
 export const dogCreatedError = (dog) => ({
   type: types.DOG_CREATED_ERROR,
   payload: dog,
+});
+
+export const dogUpdatedSuccess = (dogs) => ({
+  type: types.DOG_UPDATED_SUCCESS,
+  payload: dogs,
+});
+
+export const dogUpdatedError = (dogs) => ({
+  type: types.DOG_UPDATED_ERROR,
+  payload: dogs,
 });
 
 export const getDogs = () => (dispatch) => {
@@ -45,5 +65,29 @@ export const createDog = (dogs) => (dispatch) => {
     dispatch(dogCreatedSuccess(`${response.data.name} created`));
   }).catch((error) => {
     dispatch(dogCreatedError(error.response.data.error));
+  });
+};
+
+export const getOneDog = (id) => (dispatch) => {
+  const auth = localStorage.token;
+  axios.get(`${url}/${id}`, {
+    headers: { Authorization: `Bearer ${auth}` },
+  }).then((response) => {
+    dispatch(oneDogLoadSuccess(response.data));
+  }).catch((error) => {
+    dispatch(oneDogLoadError(error.response.data.error));
+  });
+};
+
+export const updateDog = (dog) => (dispatch) => {
+  const auth = localStorage.token;
+  const data = {
+    regular_play_time: dog.playTime,
+  };
+
+  axios.patch(`${url}/${dog.id}`, data, { headers: { Authorization: `Bearer ${auth}` } }).then((response) => {
+    dispatch(dogUpdatedSuccess(`${response.data.name} updated`));
+  }).catch((error) => {
+    dispatch(dogUpdatedError(error.response.data.error));
   });
 };
