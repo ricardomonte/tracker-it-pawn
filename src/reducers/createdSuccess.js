af5bea35e1d5b5e5319cba897fa58e45ec2dd762
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 02:50:26 +0200

    Add loginSuccess in createdReducer

--- src/reducers/createdSuccess.js
+++ src/reducers/createdSuccess.js
@@ -2,10 +2,12 @@ import * as types from '../actions/actionTypes';
 
 const createdReducer = (state = [], action) => {
   switch (action.type) {
+    case types.LOGGIN_SUCCESS:
+      return action.payload;
     case types.EXPENSE_CREATED_SUCCESS:
-      return { expenseSuccess: action.payload };
+      return action.payload;
     case types.DOG_CREATED_SUCCESS:
-      return { dogSuccess: action.payload };
+      return action.payload;
     default:
       return state;
   }
