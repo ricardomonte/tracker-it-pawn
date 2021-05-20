44f6e8cc3bce6853fbd42d1292313633baf0922c
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 22:59:53 +0200

    Change div for Link

--- src/components/MyDogs.js
+++ src/components/MyDogs.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import TrackerStyle from '../styles/MyDogs.module.css';
 
 const MyDogs = ({ dog }) => (
-  <div className={TrackerStyle.card}>
+  <Link to={`/dog/${dog.id}`} className={TrackerStyle.card}>
     <FontAwesomeIcon icon={faPaw} size="2x" className={TrackerStyle.paw} />
     <h4 className={TrackerStyle.dog}>{dog.name}</h4>
-  </div>
+  </Link>
 );
 
 MyDogs.propTypes = {
