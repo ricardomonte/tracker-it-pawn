4f92f6da3f9653d33cea497467d31691f3c5958b
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 02:44:47 +0200

    Add progressbar, fontawesome in expense component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import CreateDate from '../util/CreateDate';
+import TrackerStyle from '../styles/Expense.module.css';
 
 const Expense = ({ expense }) => (
-  <>
-    <h4>{expense.category}</h4>
-  </>
+  <div className={TrackerStyle.card}>
+    <div className={TrackerStyle.cardText}>
+      <FontAwesomeIcon icon={faCreditCard} size="2x" className={TrackerStyle.credit} />
+      <h4 className={TrackerStyle.expense}>{expense.category}</h4>
+    </div>
+    <div className={TrackerStyle.expenseProgress}>
+      <h3 className={TrackerStyle.date}>
+        {CreateDate(expense.day_of_expense, expense.month_of_expense, expense.year_of_expense)}
+      </h3>
+      <div style={{ width: '60px', height: '60px' }}>
+
+        <CircularProgressbar
+          value={expense.amount}
+          text={`${expense.amount} euros`}
+          maxValue={1000}
+        />
+      </div>
+    </div>
+  </div>
 );
 
 Expense.propTypes = {
