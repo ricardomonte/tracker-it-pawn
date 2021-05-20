fd3c3b252f4b871b45e20feeda57959c27355adb
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 23:07:55 +0200

    Add oneDog to RootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import authenticatedUser from './authorization';
 import manageErr from './manageErrors';
 import expenses from './expenses';
 import dogs from './dogs';
+import oneDog from './oneDog';
 import createdReducer from './createdSuccess';
 
 const rootReducer = combineReducers({
   authenticatedUser,
   expenses,
   dogs,
+  oneDog,
   createdReducer,
   manageErr,
 });
