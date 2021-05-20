89dcb4322aa0c106f486f931fbedbdd435ad0e3c
Ricardo Montenegro <ramn21@gmail.com>
2021-05-20 02:48:55 +0200

    Add state to display component

--- src/container/Profile.js
+++ src/container/Profile.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDog, faWallet, faPlus } from '@fortawesome/free-solid-svg-icons';
 import Expense from '../components/Expense';
 import { checkTokenUser } from '../actions/logginActions';
 import { getExpenses } from '../actions/expensesActions';
 import { getDogs } from '../actions/dogsActions';
 import ExpenseChart from '../components/ExpenseChart';
 import MyDogs from '../components/MyDogs';
+import TrackerStyle from '../styles/Profile.module.css';
 
 const Profile = ({
   user, userCheck, myExpenses, expenses, userDogs, dogs,
@@ -17,33 +20,79 @@ const Profile = ({
     myExpenses();
     userDogs();
   }, []);
+
+  const [visibleDog, setVisibleDog] = useState(false);
+  const [visibleExp, setVisibleExp] = useState(false);
+
+  const handleVisibilityDog = () => {
+    if (visibleDog === false) {
+      setVisibleDog(true);
+    } else {
+      setVisibleDog(false);
+    }
+  };
+  const handleVisibilityExp = () => {
+    if (visibleExp === false) {
+      setVisibleExp(true);
+    } else {
+      setVisibleExp(false);
+    }
+  };
+
   return (
-    <div>
-      <div>
-        <h1>Welcome</h1>
+    <div className={TrackerStyle.container}>
+      <div className={TrackerStyle.title}>
+        <h1 className={TrackerStyle.text}>Welcome</h1>
         <h3>{user.name}</h3>
       </div>
-      <div>
-        <h3>My Dogs</h3>
+      <div className={TrackerStyle.containerDogExp}>
         <div>
-          { dogs.length > 0 && dogs.map((element) => (
-            <MyDogs key={element.name} dog={element} />
-          ))}
-          <Link to="/add_dog">add your dog</Link>
+          <div className={TrackerStyle.containerPaw}>
+            <FontAwesomeIcon icon={faDog} onClick={handleVisibilityDog} role="presentation" size="5x" border className={TrackerStyle.paw} />
+            <h3 className={TrackerStyle.textDog}>My Dogs</h3>
+          </div>
+        </div>
+        <div>
+          <div className={TrackerStyle.containerPaw}>
+            <FontAwesomeIcon icon={faWallet} onClick={handleVisibilityExp} role="presentation" size="5x" border className={TrackerStyle.expense} />
+            <h3 className={TrackerStyle.textDog}>Expenses</h3>
+          </div>
         </div>
       </div>
+      { visibleDog
+        ? (
+          <div className={TrackerStyle.dogCard}>
+            { dogs.length > 0 && dogs.map((element) => (
+              <MyDogs key={element.name} dog={element} />
+            ))}
+          </div>
+        )
+        : <div />}
+      { visibleExp
+        ? (
+          <div className={TrackerStyle.dogCard}>
+            { expenses.length > 0 && expenses.map((element) => (
+              <Expense key={element.category} expense={element} />
+            ))}
+          </div>
+        )
+        : <div />}
       <div>
-        <h3>Expenses</h3>
-        <div>
-          { expenses.length > 0 && expenses.map((element) => (
-            <Expense key={element.category} expense={element} />
-          ))}
-        </div>
         <ExpenseChart expense={expenses} />
+      </div>
+      <div className={TrackerStyle.containerLinks}>
         <div>
-          <Link to="/create_expense">add expense</Link>
+          <Link className={TrackerStyle.linkDog} to="/add_dog">
+            <FontAwesomeIcon icon={faDog} size="3x" />
+            <FontAwesomeIcon icon={faPlus} size="2x" className={TrackerStyle.plus} />
+          </Link>
         </div>
-
+        <div>
+          <Link className={TrackerStyle.linkExp} to="/create_expense">
+            <FontAwesomeIcon icon={faWallet} size="3x" />
+            <FontAwesomeIcon icon={faPlus} size="2x" className={TrackerStyle.plus} />
+          </Link>
+        </div>
       </div>
     </div>
   );
