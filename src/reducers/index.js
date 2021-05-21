ea1e6cf535d158b0e0a36b51a4c274c82b33ea6a
Ricardo Montenegro <ramn21@gmail.com>
2021-05-21 06:19:29 +0200

    Add hoursDog in rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,14 @@ import expenses from './expenses';
 import dogs from './dogs';
 import oneDog from './oneDog';
 import createdReducer from './createdSuccess';
+import HoursDog from './loadHoursDog';
 
 const rootReducer = combineReducers({
   authenticatedUser,
   expenses,
   dogs,
   oneDog,
+  HoursDog,
   createdReducer,
   manageErr,
 });
